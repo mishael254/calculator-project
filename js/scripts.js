c1b1b3ab278a5970e59646192c235df5f71021ae
mishael254 <mishaelmishantoz@gmail.com>
2020-05-07 13:11:10 +0300

    created a new folder with a scripts.js file in js folder

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var subtract= function(number1,number2){
+return number1-number2;
+};
+var number1=prompt("enter a number");
+var number2=prompt("enter another number");
+alert(subtract(number1,number2));
